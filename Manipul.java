c1fbcee499fe7c1e7c51f9115ac23ed6ee8c3f60
Tms-bodnar <tms.bodnar@gmail.com>
2016-09-01 18:12:40 +0200

    Add files via upload

---
+++ Manipul.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textreversinator;
+
+import java.util.ArrayList;
+import static textreversinator.SubMondat.*;
+
+/**
+ *
+ * @author user
+ */
+public class Manipul {
+    public static void manipul(){
+            
+//        String subMondat1;
+//        //subMondat = mondat.subMondat();
+//        ArrayList<String> mondatok = new ArrayList<>(); 
+//        subMondat1 = subMondat.toString();
+//        mondatok.add(subMondat1); 
+//        System.out.println(mondatok);
+    }
+}    
